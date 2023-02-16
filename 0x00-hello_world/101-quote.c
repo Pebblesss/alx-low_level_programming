add4dd936332a91c083eefb15e520fd66b18d6c3
Pebblesss <pebblesnjuguna00@gmail.com>
2023-02-16 14:04:51 -0800

    Task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,11 @@
 #include <unistd.h>
-
 /**
- * main - Prints quote
+ * main - A c program that prints a line by Dora Korpar
  * Return: 1 (success)
  */
 int main(void)
-{	
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+{
+	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+	stdout);
 	return (1);
 }
