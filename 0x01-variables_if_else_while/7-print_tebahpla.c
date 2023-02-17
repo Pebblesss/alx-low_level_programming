af9cd527c3f4b0fae4ca5e1a62d179725734c45d
Pebblesss <pebblesnjuguna00@gmail.com>
2023-02-17 13:13:26 -0800

    Reverse alphabet

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - prints alphabet in reverse
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 'z'; i >= 'a'; i++)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
