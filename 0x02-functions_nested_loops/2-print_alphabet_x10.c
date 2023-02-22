34b59a24435990e1d1bff217c98c10568742952f
Pebblesss <pebblesnjuguna00@gmail.com>
2023-02-21 23:27:24 -0800

    Task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - prints aplhabet 10 times
+ */
+void print_alphabet_x10(void)
+{
+	int i, j;
+	char c;
+
+	for (i = 0; i < 10; i++)
+	{
+		c = 'a';
+		for (j = 0; j < 26; j++)
+		{
+			_putchar(c);
+			c++;
+		}
+		_putchar('\n')
+	}
+}
