819774851cc44b5bb67fcaeefc841e9572457863
Pebblesss <pebblesnjuguna00@gmail.com>
2023-02-22 02:20:00 -0800

    Task 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -5,11 +5,11 @@
  */
 void times_table(void)
 {
-	int i, j, result;
+	int i, j, res;
 
-	for (i = 0, i <= 9, i++)
+	for (i = 0; i <= 9; i++;)
 	{
-		for (j = 0, j <= 9, j++)
+		for (j = 0; j <= 9; j++;)
 		{
 			result = i * j;
 
