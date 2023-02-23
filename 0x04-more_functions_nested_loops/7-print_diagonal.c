bdd24a722fcbc4b4f259ffcbd1e76fbd99f6d72f
Pebblesss <pebblesnjuguna00@gmail.com>
2023-02-23 14:05:59 -0800

    7-print_diagonal.c

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -7,16 +7,17 @@
  */
 void print_diagonal(int n)
 {
-	int i, j;
-
-	for (i = 0; i < n; i++)
+	if (n <= 0)
 	{
-		for (j = 0; j < i; j++)
+		_putchar('\n');
+	}
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < i; j++)
 		{
 			_putchar(' ');
 		}
-		_putchar(92);
-		if (i < (n - 1)
-			_putchar('\n');
+		_putchar('\\');
+		_putchar('\n');
 	}
 }
