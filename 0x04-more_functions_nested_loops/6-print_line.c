9aa70b596433764a2db8d9a8b95a6c124c04a2df
Pebblesss <pebblesnjuguna00@gmail.com>
2023-02-23 13:15:38 -0800

    6-print_line.c

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -7,11 +7,17 @@
  */
 void print_line(int n)
 {
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+
 	int i;
 
-	for (i = 0; i < n; i++)
+	for (i = 0; i <  n; i++)
 	{
-		_putchar(95);
+		_putchar('_');
 	}
+
 	_putchar('\n');
 }
