c9def02709c42b291c6db808bf142f2cd658f52e
Pebblesss <pebblesnjuguna00@gmail.com>
2023-02-23 11:46:30 -0800

    files

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,17 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+int _isupper(int c);
+int _isdigit(int c);
+int mul(int a, int b);
+void print_numbers(void);
+int _putchar(char);
+void print_most_numbers(void);
+void more_numbers(void);
+void print_line(void);
+void print_diagonal(void);
+void print_square(void);
+void print_triangle(void);
+void print_number(void);
+
+#endif
