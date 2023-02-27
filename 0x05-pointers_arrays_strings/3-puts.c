c5c3ad635dc64278182f4aa0f867facfee2058fb
Pebblesss <pebblesnjuguna00@gmail.com>
2023-02-27 10:40:12 -0800

    #include <unistd.h>
    /**
     * _putchar - writes character c to stdout
     * @c: char to be printed
     * Return: On success 1
     */
    int _putchar(char c)
    {
            return (write(1, &c, 1));
    }

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
  * _puts - prints a string, followed by a new line
@@ -12,5 +11,5 @@ void _puts(char *str)
 		_putchar(*str);
 		str++;
 	}
-	_putchar("\n");
+	_putchar('\n');
 }
