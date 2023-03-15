812e22bdd2961c112e5516eb997885442b351581
Pebblesss <pebblesnjuguna00@gmail.com>
2023-03-15 10:23:00 -0700

    2-args.c

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -11,8 +11,11 @@
 
 int main(int argc, char *argv[])
 {
-	(void) argv; /*Ignore argv*/
+	int i;
 
-	printf("%d\n", argc - 1);
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
 	return (0);
 }
